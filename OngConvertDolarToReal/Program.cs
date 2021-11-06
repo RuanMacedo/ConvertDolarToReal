4acee81f97e0aa9109d627e24f1a2d740f6d06d5
Ruan Macedo <ruan.macedo@outlook.com>
2021-11-06 13:40:30 -0300

    Convert Dolar to Real

---
+++ OngConvertDolarToReal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OngConvertDolarToReal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double vlr,vlrReal,dolar;
+
+            Console.WriteLine("Bem vindo ao conversor da ONG!");
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite o valor da doação em Dolar:");
+            vlr = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Cotação atual do dólar: ");
+            dolar = Convert.ToDouble(Console.ReadLine());
+
+            vlrReal = vlr * dolar;
+
+            Console.WriteLine("Valor da doação em Real: {0}",vlrReal.ToString("0.00"));
+
+        }
+    }
+}
